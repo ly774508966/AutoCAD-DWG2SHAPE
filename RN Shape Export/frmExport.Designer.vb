a2d760f62d24e66f89364680fc5b213a130ccb61
Roy Nijkamp <roy_nijkamp@hotmail.com>
2018-04-26 17:02:11 +0200

    layer filters

--- RN Shape Export/frmExport.Designer.vb
+++ RN Shape Export/frmExport.Designer.vb
@@ -36,6 +36,8 @@ Partial Class frmExport
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.cmdHatchToBoundary = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -152,11 +154,31 @@ Partial Class frmExport
         Me.cmdHatchToBoundary.Text = "Arceringen omzetten naar Boundary"
         Me.cmdHatchToBoundary.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(389, 73)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(318, 254)
+        Me.TextBox1.TabIndex = 21
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(623, 16)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'frmExport
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(363, 347)
+        Me.ClientSize = New System.Drawing.Size(719, 347)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.cmdHatchToBoundary)
         Me.Controls.Add(Me.buttonCancel)
         Me.Controls.Add(Me.buttonOK)
@@ -170,7 +192,6 @@ Partial Class frmExport
         Me.Controls.Add(Me.Label1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(379, 386)
         Me.MinimizeBox = False
         Me.MinimumSize = New System.Drawing.Size(379, 386)
         Me.Name = "frmExport"
@@ -194,4 +215,6 @@ Partial Class frmExport
     Friend WithEvents Label2 As Windows.Forms.Label
     Friend WithEvents Label1 As Windows.Forms.Label
     Friend WithEvents cmdHatchToBoundary As Windows.Forms.Button
+    Friend WithEvents TextBox1 As Windows.Forms.TextBox
+    Friend WithEvents Button1 As Windows.Forms.Button
 End Class
--- RN Shape Export/frmExport.vb
+++ RN Shape Export/frmExport.vb
@@ -36,6 +36,15 @@ Imports Autodesk.AutoCAD.Geometry
 Imports RN_Shape_Export.ImportExport
 
 Public Class frmExport
+    Dim acDoc As Document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+    Dim acCurDb As Database = acDoc.Database
+    Dim acEd As Editor = acDoc.Editor
+    Dim commandActive As Boolean = True
+    Dim commandFailled As Boolean = False
+    Dim FailedLayers As String = ""
+    Dim currLayer As String = ""
+    Dim iCurrLayer As Integer = 0
+
     Private Sub buttonExpFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonExpFile.Click
         Dim saveFileDialog1 As SaveFileDialog = New SaveFileDialog()
 
@@ -123,103 +132,330 @@ Public Class frmExport
 
     Private Sub cmdHatchToBoundary_Click(sender As Object, e As EventArgs) Handles cmdHatchToBoundary.Click
         If MsgBox("Hiermee worden de Arceringen omgezet naar Poylylines, de originele Arceringen worden verwijderd!" & vbCrLf & "Wilt u doorgaan?", vbYesNo) = vbYes Then
-            Me.Hide()
             'set focus to modal space
-            Autodesk.AutoCAD.Internal.Utils.SetFocusToDwgView()
+            'Autodesk.AutoCAD.Internal.Utils.SetFocusToDwgView()
             'RHB()
-            LoopLayers()
-            Me.Show()
+            LoopLayers(0)
         End If
     End Sub
 
-    Public Sub LoopLayers()
+    Public Sub LoopLayers(iStart As Integer)
+        TextBox1.AppendText("Start Loop with Value: " & iStart.ToString & " - " & iCurrLayer.ToString & vbCrLf)
+        TextBox1.Update()
+        Me.Update()
         Dim sc As ImportExportCommands = New ImportExportCommands()
         Dim layerNames As StringCollection = New StringCollection()
+        Dim iTeller As Integer = 0
+        iCurrLayer = iStart
         If sc.GetLayers(layerNames) Then
+            TextBox1.AppendText("LayerCount: " & layerNames.Count.ToString & vbCrLf)
+            TextBox1.Update()
+            Me.Update()
+            If iCurrLayer = layerNames.Count Then
+                MsgBox("Voltooid")
+                Exit Sub
+            End If
             Dim layerName As String = Nothing
+            acEd.Regen()
             For Each layerName In layerNames
-                If layerName = "0" Or layerName = "Defpoints" Then
 
-                Else
-                    FreezeLayers(layerName)
-                    Exit Sub
+                If iTeller = iCurrLayer Then
+                    If layerName = "0" Or layerName = "Defpoints" Then
+                        iCurrLayer = iCurrLayer + 1
+                    Else
+                        currLayer = layerName
+                        SetCurrentLayer(currLayer)
+                        TextBox1.AppendText("Start Isolating: " & currLayer & vbCrLf)
+                        TextBox1.Update()
+                        Me.Update()
+                        If IsolateLayer(layerName) Then
+
+                        End If
+                        acEd.ApplyCurDwgLayerTableChanges()
+                        acEd.Regen()
+                        Threading.Thread.Sleep(5000)
+                        TextBox1.AppendText("Start Boundary generation: " & vbCrLf)
+                        TextBox1.Update()
+                        Me.Update()
+                        If HatchToBoundary() = True Then
+                            'success
+                            acEd.Regen()
+                        End If
+                        IsolateLayer("", True)
+                        'cancel last active command
+                        acDoc.SendStringToExecute(Chr(27), True, False, True)
+                        acEd.Regen()
+                        TextBox1.AppendText("Ending Loop: " & iStart.ToString & vbCrLf)
+                        TextBox1.Update()
+                        Me.Update()
+                        Threading.Thread.Sleep(5000)
+                        Exit Sub
+                    End If
                 End If
+                iTeller = iTeller + 1
             Next
+
+            'If DeleteHatches() Then
+            '    MsgBox("Voltooid")
+            'End If
+
         End If
     End Sub
 
-    '    Public Sub SendHatchBack()
-    '        Dim fType(0 To 1) As Integer
-    '        Dim fData(0 To 1) As Variant
-    '        fType(0) = 0 : fData(0) = "HATCH"
-    '        fType(1) = 8 : fData(1) = "0"
-
-    '        With ThisDrawing.PickfirstSelectionSet
-    '            .Clear
-    '            .Select acSelectionSetAll, , , fType, fData
-    'If .Count > 0 Then ThisDrawing.SendCommand("._DrawOrder _Previous
-    '_Back ")
-    '        End With
-    '    End Sub
-    Public Sub FreezeLayers(sLayer As String)
-        Dim sc As ImportExportCommands = New ImportExportCommands()
-        Dim layerNames As StringCollection = New StringCollection()
-        Dim acDoc As Document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
-        Dim acCurDb As Database = acDoc.Database
-        Dim acEd As Editor = acDoc.Editor
+    Public Sub SetCurrentLayer(sLayer As String)
         Using acLockDoc As DocumentLock = acDoc.LockDocument()
+            '' Start a transaction
             Using acTrans As Transaction = acCurDb.TransactionManager.StartTransaction()
-                Dim acLyrTbl As LayerTable
-                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForWrite)
-                If sc.GetLayers(layerNames) Then
-                    For Each layerName In layerNames
-                        If layerName = sLayer Then
-                            'overslaan deze moet actief blijven
-                        Else
-                            Dim acLyrTblRec As LayerTableRecord = acTrans.GetObject(acLyrTbl(sLayer), OpenMode.ForWrite)
-
-                            '' Freeze the layer
-                            acLyrTblRec.IsFrozen = True
+                '' Open the Layer table for read
+                Dim LayerTable As LayerTable = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForRead)
+                For Each ObjectId In LayerTable
+                    Dim LayerTableRecord As LayerTableRecord = acTrans.GetObject(ObjectId, OpenMode.ForWrite)
+                    Try
+                        If LayerTableRecord.Name = sLayer Then
+                            LayerTableRecord.IsOff = False
+                            LayerTableRecord.IsFrozen = False
+                            acCurDb.Clayer = LayerTable(sLayer)
+                            TextBox1.AppendText("### Set Curr Layer: " & sLayer & vbCrLf)
+                            Exit For
                         End If
+                        TextBox1.Update()
+                        Me.Update()
+                    Catch ex As Exception
 
-                    Next
-                End If
+                    End Try
+                Next
+                '' Save the changes and dispose of the transaction
                 acTrans.Commit()
             End Using
         End Using
     End Sub
 
-    Public Sub convertHatch(sLayer As String)
-        Dim acDoc As Document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
-        Dim acCurDb As Database = acDoc.Database
-        Dim acEd As Editor = acDoc.Editor
+
+    Public Function IsolateLayer(sLayer As String, Optional bUnisoAll As Boolean = False)
         Using acLockDoc As DocumentLock = acDoc.LockDocument()
+            '' Start a transaction
             Using acTrans As Transaction = acCurDb.TransactionManager.StartTransaction()
-                Dim acLyrTbl As LayerTable
-                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForRead)
-                Dim acLyrTblRec As LayerTableRecord = acTrans.GetObject(acLyrTbl(sLayer), OpenMode.ForWrite)
-
-                '' Freeze the layer
-                acLyrTblRec.IsFrozen = False
-
-                acCurDb.Clayer() = acLyrTbl(sLayer)
+                '' Open the Layer table for read
+                Dim tmpLayer As String
+                Dim LayerTable As LayerTable = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForWrite)
+                For Each ObjectId In LayerTable
+                    Dim LayerTableRecord As LayerTableRecord = acTrans.GetObject(ObjectId, OpenMode.ForWrite)
+                    Try
+                        tmpLayer = LayerTableRecord.Name
+                        If bUnisoAll = True Then
+                            LayerTableRecord.IsOff = False
+                            LayerTableRecord.IsFrozen = False
+                            acEd.WriteMessage("Curr layer: " & sLayer & vbCr)
+                        Else
+                            If LayerTableRecord.Name = sLayer Then
+                                LayerTableRecord.IsOff = False
+                                LayerTableRecord.IsFrozen = False
+                                'acCurDb.Clayer() = LayerTable(sLayer)
+                                TextBox1.AppendText("Curr Layer: " & sLayer & vbCrLf)
+                            Else
+                                LayerTableRecord.IsOff = True
+                                LayerTableRecord.IsFrozen = True
+                                TextBox1.AppendText("Off / Freeze Layer: " & LayerTableRecord.Name & vbCrLf)
+                            End If
+                        End If
+                        TextBox1.Update()
+                        Me.Update()
+                    Catch ex As Exception
+                        TextBox1.AppendText("EXCEPTION FOT LAYER: " & tmpLayer & vbCr)
+                        TextBox1.Update()
+                        Me.Update()
+                    End Try
+                Next
+                '' Save the changes and dispose of the transaction
                 acTrans.Commit()
+            End Using
+        End Using
+        listLayerState()
+        Return True
+    End Function
 
-                'select all objects on layer
-                Dim tvs As TypedValue() = New TypedValue(0) {New TypedValue(CInt(DxfCode.LayerName), sLayer)}
-                Dim sf As SelectionFilter = New SelectionFilter(tvs)
-                Dim psr As PromptSelectionResult = acEd.SelectAll(sf)
-                If psr.Status = PromptStatus.OK Then
-                    Dim oObjectIDS As ObjectIdCollection = New ObjectIdCollection(psr.Value.GetObjectIds())
+    Public Function listLayerState()
+        Using acLockDoc As DocumentLock = acDoc.LockDocument()
+            '' Start a transaction
+            TextBox1.AppendText("$$$$$ Layer State List: " & vbCrLf)
+            Using acTrans As Transaction = acCurDb.TransactionManager.StartTransaction()
+                '' Open the Layer table for read
+                Dim LayerTable As LayerTable = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForWrite)
+                For Each ObjectId In LayerTable
+                    Dim LayerTableRecord As LayerTableRecord = acTrans.GetObject(ObjectId, OpenMode.ForWrite)
+                    Try
+                        If LayerTableRecord.Name = currLayer Then
+                            TextBox1.AppendText("CURRENT ")
+                        End If
+                        TextBox1.AppendText("Layer: " & LayerTableRecord.Name & " " & LayerTableRecord.IsOff.ToString & " " & LayerTableRecord.IsFrozen.ToString & vbCrLf)
+                        TextBox1.Update()
+                        Me.Update()
+                    Catch ex As Exception
+
+                    End Try
+                Next
+                '' Save the changes and dispose of the transaction
+                acTrans.Commit()
+            End Using
+        End Using
+        Return True
+    End Function
 
 
+    Public Sub FreezeAllLayers(sLayer As String, sPrefLayer As String)
+        Using acLockDoc As DocumentLock = acDoc.LockDocument()
+            '' Start a transaction
+            Using acTrans As Transaction = acCurDb.TransactionManager.StartTransaction()
+                '' Open the Layer table for read
+                Dim LayerTable As LayerTable = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForRead)
+                For Each ObjectId In LayerTable
+                    Dim LayerTableRecord As LayerTableRecord = acTrans.GetObject(ObjectId, OpenMode.ForWrite)
+                    Try
+                        If LayerTableRecord.Name = sLayer Then
+                            LayerTableRecord.IsOff = False
+                            LayerTableRecord.IsFrozen = False
+                            'acCurDb.Clayer() = LayerTable(sLayer)
+                            TextBox1.AppendText("Curr Layer: " & sLayer & vbCrLf)
+                        Else
+                            LayerTableRecord.IsOff = True
+                            LayerTableRecord.IsFrozen = True
+                            TextBox1.AppendText("Off / Freeze Layer: " & LayerTableRecord.Name & vbCrLf)
+                        End If
+                        TextBox1.Update()
+                        Me.Update()
+                    Catch ex As Exception
+                        'TextBox1.AppendText("EXCEPTION FOT LAYER: " & tmpLayer & vbCr)
+                        'TextBox1.Update()
+                        'Me.Update()
+                    End Try
+                Next
+                '' Save the changes and dispose of the transaction
+                acTrans.Commit()
+            End Using
+        End Using
+    End Sub
+
+    Public Function HatchToBoundary()
+        '_HATCHGENERATEBOUNDARY
+        TextBox1.AppendText("Boundary for: " & currLayer & vbCrLf)
+        TextBox1.Update()
+        Me.Update()
+        AddHandlers()
+        acDoc.SendStringToExecute("_HATCHGENERATEBOUNDARY" & vbCr & "ALL" & vbCr & vbCr, True, False, True)
+        While m_Command IsNot Nothing
+
+        End While
+        TextBox1.AppendText("Sleep mode entering: " & currLayer & vbCrLf)
+        TextBox1.Update()
+        Me.Update()
+        Threading.Thread.Sleep(500)
+        TextBox1.AppendText("End boundary: " & currLayer & vbCrLf)
+        TextBox1.Update()
+        Me.Update()
+        Return True
+    End Function
+
+    Public Function DeleteHatches(Optional bGetSelectionCount As Boolean = False)
+        Dim acTypValAr(0) As TypedValue
+        acTypValAr.SetValue(New TypedValue(DxfCode.Start, "HATCH"), 0)
+
+        Dim acSelFtr As SelectionFilter = New SelectionFilter(acTypValAr)
+
+        Dim acSSPrompt As PromptSelectionResult
+        Using acLockDoc As DocumentLock = acDoc.LockDocument()
+            '' Start a transaction
+            Using acTrans As Transaction = acCurDb.TransactionManager.StartTransaction()
+                acSSPrompt = acEd.GetSelection(acSelFtr)
+                If acSSPrompt.Status = PromptStatus.OK Then
+                    Dim acSSet As SelectionSet = acSSPrompt.Value
+                    If bGetSelectionCount = True Then
+                        Return acSSet.Count
+                    Else
+                        'Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Number of objects selected: " & acSSet.Count.ToString())
+                        For Each acSSObj As SelectedObject In acSSet
+                            '' Check to make sure a valid SelectedObject object was returned
+                            If Not IsDBNull(acSSObj) Then
+                                '' Open the selected object for write
+                                Dim acEnt As Entity = acTrans.GetObject(acSSObj.ObjectId, OpenMode.ForWrite)
+
+                                If Not IsDBNull(acEnt) Then
+                                    '' Change the object's color to Green
+                                    'acEd.WriteMessage("Ent: " & acEnt.GetType.ToString & vbCr)
+                                    TextBox1.AppendText(acEnt.GetType.ToString & vbCrLf)
+                                    acEnt.Erase()
+                                End If
+                            End If
+                        Next
+                        acTrans.Commit()
+                        Return True
+                    End If
 
                 Else
-
+                    'Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Number of objects selected: 0")
+                    If bGetSelectionCount = True Then
+                        Return 0
+                    Else
+                        Return True
+                    End If
                 End If
             End Using
         End Using
+
+
+    End Function
+
+    Private m_Command As String = Nothing
+
+    Sub AddHandlers()
+        Dim d As Document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        AddHandler d.CommandWillStart, AddressOf CommandWillStart
+        AddHandler d.CommandEnded, AddressOf CommandEnded
+        AddHandler d.CommandFailed, AddressOf CommandFailed
+        AddHandler d.CommandCancelled, AddressOf CommandCancelled
+        commandFailled = False
+        commandActive = True
     End Sub
+
+    Sub RemoveHandlers()
+        Dim d As Document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        RemoveHandler d.CommandWillStart, AddressOf CommandWillStart
+        RemoveHandler d.CommandEnded, AddressOf CommandEnded
+        RemoveHandler d.CommandFailed, AddressOf CommandFailed
+        RemoveHandler d.CommandCancelled, AddressOf CommandCancelled
+        m_Command = Nothing
+        commandActive = False
+        TextBox1.AppendText("#######  Remove Handler" & vbCrLf)
+        TextBox1.Update()
+        If commandFailled = True Then
+            FailedLayers = currLayer & vbCrLf
+        End If
+        iCurrLayer = iCurrLayer + 1
+        LoopLayers(iCurrLayer)
+    End Sub
+
+    Sub CommandWillStart(ByVal sender As Object, ByVal e As Autodesk.AutoCAD.ApplicationServices.CommandEventArgs)
+        If m_Command Is Nothing Then m_Command = e.GlobalCommandName
+    End Sub
+
+    Sub CommandEnded(ByVal sender As Object, ByVal e As Autodesk.AutoCAD.ApplicationServices.CommandEventArgs)
+        If e.GlobalCommandName = m_Command Then
+
+            RemoveHandlers()
+        End If
+    End Sub
+
+    Sub CommandCancelled(ByVal sender As Object, ByVal e As Autodesk.AutoCAD.ApplicationServices.CommandEventArgs)
+        commandFailled = True
+        RemoveHandlers()
+    End Sub
+
+    Sub CommandFailed(ByVal sender As Object, ByVal e As Autodesk.AutoCAD.ApplicationServices.CommandEventArgs)
+        commandFailled = True
+        RemoveHandlers()
+    End Sub
+
+
+
 
     Public Sub RHB()
         'Source: https://forums.autodesk.com/t5/net/restore-hatch-boundaries-if-they-have-been-lost-with-net/m-p/3779514#M33429
@@ -349,5 +585,9 @@ Public Class frmExport
 
             tr.Commit()
         End Using
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
     End Sub
 End Class
